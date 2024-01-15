a381571680bd35ec1c398308f8428c20cf59d698
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2024-01-15 08:55:54 +0530

    constants: Increase icon size

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,17 @@
 function loadConstants() {
   const FRAMERATE = 30;
 
-  let CANVAS_WIDTH, CANVAS_HEIGHT;
+  let CANVAS_WIDTH, CANVAS_HEIGHT, PARTICLE_RADIUS;
 
   if (window.screen.width <= 600) {
     CANVAS_WIDTH = Math.round((5 * window.screen.width) / 6);
     CANVAS_HEIGHT = Math.round((7 * CANVAS_WIDTH) / 5);
+    PARTICLE_RADIUS = Math.round((CANVAS_HEIGHT * CANVAS_WIDTH) / 7500)
   } else {
     CANVAS_WIDTH = Math.round((3 * window.screen.width) / 5);
     CANVAS_HEIGHT = Math.round((3 * CANVAS_WIDTH) / 5);
+    PARTICLE_RADIUS = Math.round((CANVAS_HEIGHT * CANVAS_WIDTH) / 15000)
   }
-
-  const PARTICLE_RADIUS = Math.round((CANVAS_HEIGHT * CANVAS_WIDTH) / 15000);
 
   const URL_PREFIX =
     "https://raw.githubusercontent.com/Tarun-Sri-Sai/RPS-Royale/main/assets";

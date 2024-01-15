1d357a533dca7c0cbc670ae9e6f9248d9ee0e070
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2024-01-15 08:41:49 +0530

    constants: Adjust parameters

--- src/constants.js
+++ src/constants.js
@@ -1,20 +1,20 @@
 function loadConstants() {
-  const PARTICLE_RADIUS = 30;
-
   const FRAMERATE = 30;
 
   const CANVAS_WIDTH = Math.round(3 * window.screen.width / 5);
   const CANVAS_HEIGHT = Math.round(3 * CANVAS_WIDTH / 5);
 
+  const PARTICLE_RADIUS = Math.round((CANVAS_HEIGHT * CANVAS_WIDTH) / 15000);
+
   const URL_PREFIX = 'https://raw.githubusercontent.com/Tarun-Sri-Sai/RPS-Royale/main/assets'
 
   constants = {
     PARTICLE_RADIUS: PARTICLE_RADIUS,
-    VISIBILITY_RADIUS: PARTICLE_RADIUS * 3,
-    PROXIMITY_RADIUS: PARTICLE_RADIUS + 2,
+    VISIBILITY_RADIUS: PARTICLE_RADIUS * 2,
+    PROXIMITY_RADIUS: PARTICLE_RADIUS,
 
     VELOCITY_LIMIT: 180 / FRAMERATE,
-    ACCELERATION_LIMIT: 6 / FRAMERATE,
+    ACCELERATION_LIMIT: 5 / FRAMERATE,
 
     CANVAS_WIDTH: CANVAS_WIDTH,
     CANVAS_HEIGHT: CANVAS_HEIGHT,
@@ -26,7 +26,7 @@ function loadConstants() {
     },
     BACKGROUND_GRAYSCALE: 220,
 
-    FLOCK_POPULATION: Math.round((CANVAS_HEIGHT * CANVAS_WIDTH) / 15000),
+    FLOCK_POPULATION: 25,
 
     FRAMERATE: FRAMERATE,
 

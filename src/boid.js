c11923bbbb594f5873b197ea46de069976a2bf8b
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2024-01-15 12:20:00 +0530

    boid: Add exponential separation

--- src/boid.js
+++ src/boid.js
@@ -121,7 +121,7 @@ class Boid {
 
     for (const boid of boids) {
       const difference = p5.Vector.sub(this.position, boid.position);
-      difference.div(Math.max(this.getDistance(boid), 1e-6) ** 1.5);
+      difference.div(1.01 ** Math.max(this.getDistance(boid), 1e-6));
 
       averagePosition.add(difference);
     }
